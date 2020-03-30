18df6aae6552381660cbe6c6f83d992d947ddaba
Tilkeii <florian@gouarin.info>
2020-03-30 11:53:01 +0200

    Exercice 3 - Installation de Jest

---
+++ src/pokemon.spec.ts
@@ -0,0 +1,7 @@
+import { Pokemon } from './pokemon';
+
+describe('Test true', () => {
+    it('Test true', () => {
+        expect(true).toBe(true);
+    })
+})
--- src/pokemon.ts
+++ src/pokemon.ts
@@ -1,9 +1,10 @@
-class Pokemon {
+export class Pokemon {
     name: string;
     constructor(name: string) {
         this.name = name;
     }
 }
 
-let pikachu = new Pokemon("Pikachu");
-console.log("Name : ", pikachu.name);
+export function wichPokemonStarts() {
+    
+}
